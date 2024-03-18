512172a4ff3d08d1e18f7794db05fa1f3bc1e0d2
DatDX <dinhxuandat@teams.airlab.dev>
2024-03-18 13:33:04 +0700

    update detectLink pipe

--- src/app/pipes/detect-link.pipe.ts
+++ src/app/pipes/detect-link.pipe.ts
@@ -7,6 +7,7 @@ export class DetectLinkPipe implements PipeTransform {
 
   transform(text: string): string {
     const pattern = /(^|\s)((?:https?:\/\/)?[\w-]+(?:\.[\w-]+)+\.?(:\d+)?(?:\/\S*)?)/gi;
+    // NOSONAR  https://stackoverflow.com/questions/61478960/sonarqube-showing-regular-expression-denial-of-service-redos
     return text?.replace(pattern, function(match, prefix, url) {
       const fullURL = (url.startsWith('http') ? '' : 'https://') + url;
       return prefix + '<a href="' + fullURL + '" target="_blank">' + url + '</a>';
